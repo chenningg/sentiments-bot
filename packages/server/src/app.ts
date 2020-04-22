9dcf0e85809edc36ba6420822e01ce1bb5dd5554
Chen Ning <chenningg97@gmail.com>
2020-04-22 12:27:52 +0800

    Updated config and dependencies

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,21 +1,40 @@
-import express, { Application, Request, Response, NextFunction } from "express";
+import express from "express";
+import bodyParser from "body-parser";
+import dotEnvExtended from "dotenv-extended";
 
-// Initialize express server
-const app: Application = express();
+// Initialize express server, port and environment variables
+const app = express();
+const config = dotEnvExtended.load();
+const port = config.SERVER_PORT || 4000; // Default to port 4000
+
+// Setup middleware to parse POST requests for url html data and JSON
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // ====== Routes ======
 
-// Index
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, this is a test!");
+// Index and home
+app.get("/", (req, res) => {
+  res.send("Hello, this is a test!!!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Hello, this is a test!!!");
+});
+
+// Test
+app.get("/api/v1/test", (req, res) => {
+  res.send({
+    weather: "rainy.",
+  });
 });
 
 // Stock page
-app.get("/stock/:name", (req: Request, res: Response) => {
+app.get("/stock/:name", (req, res) => {
   res.send(`Finding stock sentiments for ${req.params.name}`);
 });
 
-// Listen on port 4000
-app.listen(4000, () => {
-  console.log("Express server listening on port 4000.");
+// Start server, listening on provided port (default 4000)
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}.`);
 });
